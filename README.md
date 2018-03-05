0c4ce357d9df9ee4f5bacf8e35c95d7e2d6c82ff
Colin Wong <ccwongvr@gmail.com>
2018-03-05 11:40:48 -0500

    Adding py file and amending readme

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
-logs-analysis
+# Udacity Logs Analysis
+
+A simple python tool to interpret data from an internal database.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+You will need:
+
+```
+Python 3
+Vagrant
+Udacity News Database
+```
+## Running the script
+
+Do the following to run the analysis tool
+
+### Getting into Postgresql
+
+Open cmd line where vagrant is installed and run the VM
+
+```
+vagrant up
+```
+
+Log in to the VM
+
+```
+vagrant ssh
+```
+
+Enter Postgresql and the news DB
+
+```
+psql news
+```
+
+### Views
+
+You will need two views to run the python script:
+
+```
+create view total_status as
+select time::date as day, count(status) as t_count from log group by day;
+```
+
+and
+
+```
+create view error_status as
+select time::date as day, count(status) as e_count from log where status like '%404%' group by day;
+```
+
+These can simply be pasted into the cmd line when in psql and executed.
+
+### Running the script
+
+To run our python script, first exit psql
+
+```
+ctrl + D
+```
+
+Move into the shared vagrant folder
+
+```
+cd /vagrant
+```
+
+Run the script
+
+```
+python logsanalysis.py
+```
+
+## Acknowledgments
+
+* Udacity
+* Perry McManis for sql syntax help
---
+++ logsanalysis.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+#
+# An internal tool to analyze data for a new website
+
+# allows connection to server
+import psycopg2
+
+# name the database
+DBNAME = "news"
+
+
+def run_tool():
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    # executes sql query 1
+    c.execute(
+        "select articles.title, count(log.path) as views from articles "
+        "join log on log.path like concat('%', articles.slug, '%') "
+        "group by articles.title, log.path order by views desc limit 3;")
+    # stores result as var
+    top_articles = c.fetchall()
+    # executes sql query 2
+    c.execute(
+        "select authors.name, count(log.path) as views "
+        "from authors join articles on authors.id = articles.author "
+        "join log on log.path like concat('%', articles.slug, '%') "
+        "group by authors.name "
+        "order by views desc;")
+    # stores result as var
+    top_authors = c.fetchall()
+    # executes sql query 3
+    c.execute(
+        "select time::date as day from log join total_status on "
+        "total_status.day = time::date join error_status on "
+        "error_status.day = time::date where "
+        "round(100 * error_status.e_count / total_status.t_count, 2) > .01 "
+        "group by time::date, error_status.e_count, total_status.t_count;")
+    # stores result as var
+    bug_days = c.fetchall()
+    db.close()
+    # print out query results
+    print("Top Articles: (title / views)")
+    for x in range(len(top_articles)):
+        print (top_articles)[x]
+    print ("\n")
+    print("Top Authors: (name / views)")
+    for x in range(len(top_authors)):
+        print (top_authors)[x]
+    print ("\n")
+    print("Buggy Days: (date)")
+    for x in range(len(bug_days)):
+        print (bug_days)[x]
+# executes our method
+run_tool()
